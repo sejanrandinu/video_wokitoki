644c6ccd2e8854b5c58e0d52e2671982d1b3cf4a
Sejan Randinu <sejanrandinu01@gmail.com>
2026-03-12 18:05:58 +0530

    t

--- server/database.js
+++ server/database.js
@@ -2,7 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Connect to SQLite database
-const dbPath = path.resolve(__dirname, 'database.sqlite');
+// Note: .data folder on Glitch is persistent
+const dbPath = process.env.PROJECT_DOMAIN 
+    ? path.resolve(__dirname, '../.data/database.sqlite')
+    : path.resolve(__dirname, 'database.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error connecting to database:', err.message);
--- server/server.js
+++ server/server.js
@@ -143,7 +143,7 @@ app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
